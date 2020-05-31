9838669f6f6f69fe36b2a61dec3c794994da0d33
mahmoud gomaa <gomaamahmoud922@gmail.com>
2020-05-31 13:51:04 -0500

    deploy as war

--- src/main/java/edu/miu/testjenkins/TestJenkinsApplication.java
+++ src/main/java/edu/miu/testjenkins/TestJenkinsApplication.java
@@ -4,9 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestJenkinsApplication {
+public class TestJenkinsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TestJenkinsApplication.class, args);
